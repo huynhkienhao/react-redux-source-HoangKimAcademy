0c4f859661b68e4f010a9760df33ad9a4c0b7719
Huynh Kien Hao <haohk.hkits@gmail.com>
2025-01-09 22:40:15 +0700

    Học xong bài 11

---
+++ 11_lam_quen_map/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+
+function App() {
+  
+  const arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const outputMap = arrayNumber.map((item, index) => {
+    return <li key={index}>Số: {item}</li>
+  })
+
+  const fruits = ['Apple', 'Banana', 'Cherry'];
+
+
+  return (
+    <div className='App'>
+      <ul>
+        {outputMap}
+        <hr/>
+        {fruits.map((fruit, index) => {
+          return <li key={index}>{fruit}</li>
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
